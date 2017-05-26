b38a5dcff30c3ff1fdaa9fded7157939ffa2a7eb
Abigail Shirey <abigail.shirey@ge.com>
2017-05-26 15:01:37 -0500

    contains strings to test trufflehog

---
+++ highEntropyStrings.cpp
@@ -0,0 +1,12 @@
+#include<string>
+
+using namespace std
+
+int main(){
+	String high = "23daU0xvh0adfjlgdaw3BDShcdsl3_kio7SD1C0Z";
+	String low = "hi i'm abby";
+	String longlow = "this is a long string but hopefully with low shannon entropy";
+
+	return 0;
+}
+	
