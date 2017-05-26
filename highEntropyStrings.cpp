dcc2a84136058c21f2ca8cca79147f61bb7a8702
Abigail Shirey <abigail.shirey@ge.com>
2017-05-26 15:31:57 -0500

    new string added

--- highEntropyStrings.cpp
+++ highEntropyStrings.cpp
@@ -3,6 +3,7 @@
 using namespace std
 
 int main(){
+	String base64 = "ABCDEFGHIJKLKNOPQRSTUVWXYZ1234567890";
 	String high = "23daU0xvh0adfjlgdaw3BDShcdsl3_kio7SD1C0Z";
 	String low = "hi i'm abby";
 	String longlow = "this is a long string but hopefully with low shannon entropy";
